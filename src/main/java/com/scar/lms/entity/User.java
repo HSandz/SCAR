2ef3b9bcfda34857ee827ffd030661a4ac7dfa9d
minhtran1015 <143114074+minhtran1015@users.noreply.github.com>
2024-10-24 19:38:52 +0700

    Fix foreign key for role_id

--- src/main/java/com/scar/lms/entity/User.java
+++ src/main/java/com/scar/lms/entity/User.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
 @Entity
 @Getter @Setter @NoArgsConstructor
 @Table(name="users")
@@ -15,8 +14,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "email")
     private String email;
 
 }
--- src/main/resources/database/changelog/schema.sql
+++ src/main/resources/database/changelog/schema.sql
@@ -5,9 +5,10 @@ CREATE TABLE IF NOT EXISTS roles (
 
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    role VARCHAR(20) NOT NULL,
-    FOREIGN KEY (id) REFERENCES roles(id)
+    username VARCHAR(20) NOT NULL UNIQUE,
+    password VARCHAR(72) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id)
 );
+
