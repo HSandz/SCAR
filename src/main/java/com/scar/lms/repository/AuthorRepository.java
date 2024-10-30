fb7e529853a5cb338c7a0adb3986d6d0a85822ff
HSandz <lehoangsand55@gmail.com>
2024-10-30 21:11:07 +0700

    update repositories

--- src/main/java/com/scar/lms/repository/AuthorRepository.java
+++ src/main/java/com/scar/lms/repository/AuthorRepository.java
@@ -4,7 +4,15 @@ import com.scar.lms.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
 
+    List<Author> findByCountry(String country);
+
+    List<Author> findByAge(Integer age);
+
+    Optional<Author> findByEmail(String email);
 }
