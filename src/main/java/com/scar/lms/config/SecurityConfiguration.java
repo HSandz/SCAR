6aafdc3a1f8dd4f414d637d3355dc75f2c401717
minhtran1015 <143114074+minhtran1015@users.noreply.github.com>
2024-11-08 13:46:59 +0700

    Login debug and fixes

--- src/main/java/com/scar/lms/config/SecurityConfiguration.java
+++ src/main/java/com/scar/lms/config/SecurityConfiguration.java
@@ -1,14 +1,35 @@
 package com.scar.lms.config;
 
+import com.scar.lms.service.AuthenticationService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import java.util.Set;
 
 @Configuration
 public class SecurityConfiguration {
+
+    private final AuthenticationService authenticationService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public SecurityConfiguration(AuthenticationService authenticationService,
+                                 BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.authenticationService = authenticationService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -29,6 +50,7 @@ public class SecurityConfiguration {
                         .loginPage("/login")
                         .usernameParameter("username")
                         .passwordParameter("password")
+                        .successHandler(roleBasedAuthenticationSuccessHandler())
                         .permitAll())
                 .logout(logout -> logout
                         .logoutUrl("/logout")
@@ -45,6 +67,33 @@ public class SecurityConfiguration {
                 System.out.printf("User %s logged out%n", authentication.getName());
             } else {
                 System.out.println("Unable to logout due to null authentication");
+            }
+        };
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(authenticationService);
+        authProvider.setPasswordEncoder(bCryptPasswordEncoder);
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler roleBasedAuthenticationSuccessHandler() {
+        return (_, response, authentication) -> {
+            Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+            if (roles.contains("ROLE_ADMIN")) {
+                response.sendRedirect("/admin");
+            } else if (roles.contains("ROLE_USER")) {
+                response.sendRedirect("/home");
+            } else {
+                response.sendRedirect("/");
             }
         };
     }
--- src/main/java/com/scar/lms/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/scar/lms/exception/handler/GlobalExceptionHandler.java
@@ -1,9 +1,7 @@
 package com.scar.lms.exception.handler;
 
-import com.scar.lms.exception.LogoutException;
 import com.scar.lms.exception.NotFoundException;
 
-import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,13 +15,6 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(notFoundException.getMessage());
-    }
-
-    @ExceptionHandler(LogoutException.class)
-    public ResponseEntity<String> handleLogoutException(LogoutException logoutException) {
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(logoutException.getMessage());
     }
 
 }
