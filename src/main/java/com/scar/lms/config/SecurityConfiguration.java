411d449ac28870a9382bcba989175897330115c3
minhtran1015 <143114074+minhtran1015@users.noreply.github.com>
2024-11-08 12:31:47 +0700

    Update SecurityConfiguration.java

--- src/main/java/com/scar/lms/config/SecurityConfiguration.java
+++ src/main/java/com/scar/lms/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.scar.lms.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
@@ -12,19 +13,23 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .requestMatchers("/",
                                 "/register",
-                                "/home",
+                                "/login",
                                 "/css/**",
-                                "/image/**",
+                                "/media/**",
                                 "/static/**").permitAll()
-                        .requestMatchers("/books/**", "/home").hasAnyAuthority("USER", "ADMIN")
-                        .requestMatchers("/admin/**").hasAuthority("ADMIN")
+                        .requestMatchers("/books/**", "/home").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
+                        .requestMatchers("/admin/**", "/home").hasAuthority("ROLE_ADMIN")
                         .anyRequest().authenticated()
                 )
                 .formLogin(formLogin -> formLogin
-                        .loginPage("/login").permitAll())
+                        .loginPage("/login")
+                        .usernameParameter("username")
+                        .passwordParameter("password")
+                        .permitAll())
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessHandler(userLogoutSuccessHandler())
