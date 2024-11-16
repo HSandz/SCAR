24a6bded4b5d7f35fbbde653c5780136b424cad4
minhtran1015 <143114074+minhtran1015@users.noreply.github.com>
2024-11-16 11:42:34 +0700

    add exception and error pages

--- src/main/java/com/scar/lms/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/scar/lms/exception/handler/GlobalExceptionHandler.java
@@ -1,56 +1,51 @@
 package com.scar.lms.exception.handler;
 
 import com.scar.lms.exception.DuplicateResourceException;
-import com.scar.lms.exception.InvalidDataException;
+import com.scar.lms.exception.ForbiddenException;
 import com.scar.lms.exception.OperationNotAllowedException;
 import com.scar.lms.exception.ResourceNotFoundException;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> handleGenericException(Exception ex) {
-        ex.printStackTrace();
-        String errorMessage = "An unexpected error occurred: " + ex.getMessage();
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(errorMessage);
-    }
-
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<?> handleResourceNotFoundException(
-            ResourceNotFoundException resourceNotFoundException) {
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(resourceNotFoundException.getMessage());
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleResourceNotFoundException(ResourceNotFoundException ex, Model model) {
+        model.addAttribute("errorMessage", ex.getMessage());
+        return "error/404";
     }
 
     @ExceptionHandler(DuplicateResourceException.class)
-    public ResponseEntity<?> handleDuplicateResourceException(
-            DuplicateResourceException duplicateResourceException) {
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(duplicateResourceException.getMessage());
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public String handleDuplicateResourceException(DuplicateResourceException ex, Model model) {
+        model.addAttribute("errorMessage", ex.getMessage());
+        return "error/409";
     }
 
-    @ExceptionHandler(InvalidDataException.class)
-    public ResponseEntity<?> handleInvalidDataException(
-            InvalidDataException invalidDataException) {
-        return ResponseEntity
-                .status(HttpStatus.CONFLICT)
-                .body(invalidDataException.getMessage());
+    @ExceptionHandler(ForbiddenException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public String handleForbiddenException(ForbiddenException ex, Model model) {
+        model.addAttribute("errorMessage", ex.getMessage());
+        return "error/403";
     }
 
     @ExceptionHandler(OperationNotAllowedException.class)
-    public ResponseEntity<?>  handleOperationNotAllowedException(
-            OperationNotAllowedException operationNotAllowedException) {
-        return ResponseEntity
-                .status(HttpStatus.FORBIDDEN)
-                .body(operationNotAllowedException.getMessage());
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public String handleOperationNotAllowedException(OperationNotAllowedException ex, Model model) {
+        model.addAttribute("errorMessage", ex.getMessage());
+        return "error/405";
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleGenericException(Exception ex, Model model) {
+        model.addAttribute("errorMessage", "An unexpected error occurred. Please try again later.");
+        return "error/500";
     }
 }
