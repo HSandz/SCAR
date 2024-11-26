320487063fcf5ff44cce96ada4071186b08fa05a
tamngo179 <23020568@vnu.edu.vn>
2024-11-27 02:47:08 +0700

    update

--- src/main/java/com/scar/lms/entity/Book.java
+++ src/main/java/com/scar/lms/entity/Book.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.NonNull;
 import org.hibernate.proxy.HibernateProxy;
 
 import java.util.HashSet;
