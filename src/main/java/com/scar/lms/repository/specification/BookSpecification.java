d79617e13bd6812a421447131046b9db4fa2d254
HSandz <lehoangsand55@gmail.com>
2024-11-28 00:20:27 +0700

    fix

--- src/main/java/com/scar/lms/repository/specification/BookSpecification.java
+++ src/main/java/com/scar/lms/repository/specification/BookSpecification.java
@@ -1,25 +1,24 @@
 package com.scar.lms.repository.specification;
 
 import com.scar.lms.entity.Book;
-
 import org.springframework.data.jpa.domain.Specification;
 
 public class BookSpecification {
 
     public static Specification<Book> hasTitle(String title) {
-        return (root, _, criteriaBuilder) -> criteriaBuilder.equal(root.get("title"), title);
+        return (root, _, criteriaBuilder) -> criteriaBuilder.like(root.get("title"), "%" + title + "%");
     }
 
     public static Specification<Book> hasAuthor(String authorName) {
-        return (root, _, criteriaBuilder) -> criteriaBuilder.equal(root.join("authors").get("name"), authorName);
+        return (root, _, criteriaBuilder) -> criteriaBuilder.like(root.join("authors").get("name"), "%" + authorName + "%");
     }
 
     public static Specification<Book> hasGenre(String genreName) {
-        return (root, _, criteriaBuilder) -> criteriaBuilder.equal(root.join("genres").get("name"), genreName);
+        return (root, _, criteriaBuilder) -> criteriaBuilder.like(root.join("genres").get("name"), "%" + genreName + "%");
     }
 
     public static Specification<Book> hasPublisher(String publisherName) {
-        return (root, _, criteriaBuilder) -> criteriaBuilder.equal(root.join("publishers").get("name"), publisherName);
+        return (root, _, criteriaBuilder) -> criteriaBuilder.like(root.join("publishers").get("name"), "%" + publisherName + "%");
     }
 
     public static Specification<Book> hasYear(Integer year) {
